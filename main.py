69c9801888aca7982dfeda0e54498ba4d3f76cf0
Damian <damian.plomp@student.hu.nl>
2023-03-15 14:54:11 +0100

    connections made

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
 import pymongo
+import psycopg2
 from pymongo import MongoClient
 import json
 
-client = MongoClient("mongodb+srv://damianplomp:Np2NrQqUrUM8PyBz@cluster0.wmvdte3.mongodb.net/?retryWrites=true&w=majority")
-db = client['SP-Python']
+client = MongoClient("mongodb+srv://damianplomp:1234@data-webshop-sp.bsobel2.mongodb.net/test")
+db = client['huwebshop']
 
-collection = db['webshop-data']
+collection = db['product']
 
+connection = psycopg2.connect(host='postgres', database='webshop_db', user='postgres', password='1824')
 
-# First product in database
-first_item_name = collection.find()[0]['category']['category_1']
-first_item_price = collection.find()[0]['price']['selling_price']
-print(f'Het eerste product uit de database is een {first_item_name}, de prijs hiervan is {first_item_price} euro')
+cur = connection.cursor()
 
-
-
-# First product that starts with letter 'R'
-res = []
-for row in collection.find():
-    product = row['category']['category_1']
-    if product[0] == 'R':
-        print(product)
-        res.append(product)
-if len(res) == 0:
-    print('There were no items found that start with a R.')
-
-# Average price of all products
-prices = []
-for row in collection.find():
-    prices.append(row['price']['selling_price'])
-print(f'The average price of all the products is {(sum(prices)) // (len(prices))}')
